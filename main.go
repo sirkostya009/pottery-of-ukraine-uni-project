7777ffb0097be6290e403a16390a7ff5df383759
Constantine <kosta.tovstik@gmail.com>
2023-12-13 21:53:58 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
 	r.HTMLRender = &TemplRenderer{}
-	r.GET("/ping", func(c *gin.Context) {
-		c.HTML(200, "index.html", index())
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "", index())
 	})
-	_ = r.Run(":8080")
+	_ = r.Run("0.0.0.0:" + os.Getenv("PORT"))
 }
