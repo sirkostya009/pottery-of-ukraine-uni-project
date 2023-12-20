c3b6076cd1a4732dc15ab4377609b9823facdcaa
Constantine <kosta.tovstik@gmail.com>
2023-12-20 14:54:11 +0200

    Minor improvement to styles

--- templates_templ.go
+++ templates_templ.go
@@ -56,7 +56,7 @@ func navBar() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav><a href=\"/\"><img src=\"/static/лого.svg\" alt=\"Лого\"></a> <a href=\"/decor\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav class=\"centered\"><a href=\"/\"><img src=\"/static/лого.svg\" alt=\"Лого\"></a> <a href=\"/decor\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
